4142ef038dd75fb61e0fc263f058f7847974394b
Lucas N. Ferreira <lucasnfe@gmail.com>
2020-05-22 11:27:54 -0700

    Fixed training infinite loop problem

--- src/clf_lstm.py
+++ src/clf_lstm.py
@@ -8,27 +8,27 @@ BUFFER_SIZE=10000
 def build_dataset_lstm(episodes, vocabulary, context_size, batch_size, test_ep):
     (X_train, Y_train), (X_test, Y_test) = build_dataset(episodes, vocabulary, context_size, test_ep)
 
-    train_dataset = []
+    train_examples = []
     for i in range(len(X_train)):
         # Tokenize train sentence
         X_train[i] = [vocabulary[w] for w in X_train[i].split()]
 
         # add tokenized sentence to the train dataset
-        train_dataset.append((X_train[i], [Y_train[i]]))
+        train_examples.append((X_train[i], [Y_train[i]]))
 
-    train_dataset = tf.data.Dataset.from_generator(lambda: train_dataset, (tf.int32, tf.int32))
+    train_dataset = tf.data.Dataset.from_generator(lambda: iter(train_examples), (tf.int32, tf.int32))
     train_dataset = train_dataset.shuffle(BUFFER_SIZE)
     train_dataset = train_dataset.padded_batch(batch_size, padded_shapes=([None], [None]))
 
-    test_dataset = []
+    test_examples = []
     for i in range(len(X_test)):
         # Tokenize test sentence
         X_test[i] = [vocabulary[w] for w in X_test[i].split()]
 
         # add tokenized sentence to the test dataset
-        test_dataset.append((X_test[i], [Y_test[i]]))
+        test_examples.append((X_test[i], [Y_test[i]]))
 
-    test_dataset = tf.data.Dataset.from_generator(lambda: test_dataset, (tf.int32, tf.int32))
+    test_dataset = tf.data.Dataset.from_generator(lambda: iter(test_examples), (tf.int32, tf.int32))
     test_dataset = test_dataset.padded_batch(batch_size, padded_shapes=([None], [None]))
 
     return train_dataset, test_dataset
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='midi_generator.py')
     parser.add_argument('--data', type=str, required=True, help="Dnd data.")
     parser.add_argument('--ctx', type=int, default=10, help="Context window size.")
-    parser.add_argument('--batch', type=int, default=32, help="Context window size.")
+    parser.add_argument('--batch', type=int, default=32, help="Batch size.")
     opt = parser.parse_args()
 
     # Load episodes in a dictionary
@@ -55,13 +55,13 @@ if __name__ == "__main__":
         train_dataset, test_dataset = build_dataset_lstm(episodes, vocabulary, opt.ctx, opt.batch, test_ep=ep)
 
         clf_lstm = tf.keras.Sequential([
-            tf.keras.layers.Embedding(len(vocabulary), 64),
-            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
-            tf.keras.layers.Dense(64, activation='relu'),
-            tf.keras.layers.Dense(1)
+            tf.keras.layers.Embedding(len(vocabulary), 256),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256)),
+            tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dense(4)
         ])
 
-        clf_lstm.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+        clf_lstm.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                     optimizer=tf.keras.optimizers.Adam(1e-4), metrics=['accuracy'])
 
         history = clf_lstm.fit(train_dataset, epochs=10, validation_data=test_dataset)
