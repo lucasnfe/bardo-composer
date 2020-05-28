b5664e19cc23b2f86059c789f6f8fecd58141803
Lucas N. Ferreira <lucasnfe@gmail.com>
2020-05-28 13:26:57 -0700

    Use keras fit functions

--- src/clf_vgmidi/clf_gpt2.py
+++ src/clf_vgmidi/clf_gpt2.py
@@ -8,12 +8,26 @@ import tensorflow as tf
 import transformers as tm
 import midi.encoder as me
 
-from gpt2.models import *
+BUFFER_SIZE=10000
 
-def _split_input_target(chunk):
-    input_text = chunk[:-1]
-    target_text = chunk[1:]
-    return input_text, target_text
+class GPT2Classifier(tm.modeling_tf_gpt2.TFGPT2Model):
+    def __init__(self, config, num_labels=1, *inputs, **kwargs):
+        super().__init__(config, *inputs, **kwargs)
+
+        # self.dropout = tf.keras.layers.Dropout(0.5)
+        self.emotion_head = tf.keras.layers.Dense(num_labels)
+
+    def call(self, inputs, **kwargs):
+        # Extract features
+        outputs = super().call(inputs, **kwargs)
+
+        #  Extract language model logits
+        # lm_logits = self.transformer.wte(outputs[0], mode="linear", training=kwargs["training"])
+
+        # Finetuner Emotion Head
+        emotion_logits = self.emotion_head(outputs[0], training=kwargs["training"])
+
+        return emotion_logits
 
 def load_dataset(datapath, vocab, seq_length):
     dataset = []
@@ -26,95 +40,17 @@ def load_dataset(datapath, vocab, seq_length):
         piece_text = me.load_file(piece_path).split(" ")
         tokens = [vocab[c] for c in piece_text]
 
-        if len(tokens) < seq_length - 1:
-            input_text, target_text = _split_input_target(tokens)
-            dataset.append((input_text, target_text, [label]))
+        dataset.append((tokens, [label]))
 
     return dataset
 
-def _split_input_target(chunk):
-    input_text = chunk[:-1]
-    target_text = chunk[1:]
-    return input_text, target_text
-
-def build_dataset(dataset, batch_size, BUFFER_SIZE=10000):
+def build_dataset(dataset, batch_size):
     # Read all files in the dataset directory and combine them
-    tf_dataset = tf.data.Dataset.from_generator(lambda: dataset, (tf.int32, tf.int32, tf.int32))
-    tf_dataset = tf_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-
+    tf_dataset = tf.data.Dataset.from_generator(lambda: dataset, (tf.int32, tf.int32))
     tf_dataset = tf_dataset.shuffle(BUFFER_SIZE)
-    tf_dataset = tf_dataset.padded_batch(batch_size, padded_shapes=([None],[None], [1]), padding_values=(1, 1, 1))
+    tf_dataset = tf_dataset.padded_batch(batch_size, padded_shapes=([None], [1]), padding_values=(1, 1))
 
     return tf_dataset
-
-def loss(y_hat1, y_hat2, y1, y2, alpha=0.5):
-    l1 = tf.keras.losses.sparse_categorical_crossentropy(y1, y_hat1, from_logits=True)
-    l2 = tf.keras.losses.sparse_categorical_crossentropy(y2, y_hat2, from_logits=True)
-
-    return tf.expand_dims(l2, axis=1) + alpha * l1
-
-def evaluate(sentiment_model, test_dataset):
-    test_accuracy = tf.keras.metrics.BinaryAccuracy()
-
-    for x, y1, y2 in test_dataset:
-        # training=False is needed only if there are layers with different
-        # behavior during training versus inference (e.g. Dropout).
-        y_hat2, _ = sentiment_model(x, training=False)
-        test_accuracy(y2, tf.math.sigmoid(y_hat2[:, -1, :]))
-
-    acc = test_accuracy.result()
-    print("Test set accuracy: {:.3%}".format(acc))
-
-    return acc
-
-def fit(clf_gpt2, train_dataset, test_dataset, params):
-    optimizer = tf.keras.optimizers.Adam(learning_rate=params["lr"])
-
-    # Keep results for plotting
-    train_loss_results = []
-    train_accuracy_results = []
-
-    best_accuracy = 0
-    for epoch in range(params["epochs"]):
-        epoch_loss_avg = tf.keras.metrics.Mean()
-        epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
-
-        # Training loop - using batches of 32
-        for step, (x, y1, y2) in enumerate(train_dataset):
-            # Optimize the model
-            with tf.GradientTape() as tape:
-                # training=training is needed only if there are layers with different
-                # behavior during training versus inference (e.g. Dropout).
-                y_hat2, y_hat1 = clf_gpt2(x, training=True)
-                loss_value = loss(y_hat1, y_hat2[:, -1, :], y1, y2, params["alpha"])
-
-            grads = tape.gradient(loss_value, clf_gpt2.trainable_weights)
-            optimizer.apply_gradients(zip(grads, clf_gpt2.trainable_weights))
-
-            # Track loss progress
-            epoch_loss_avg(loss_value)
-
-            # Track accuracy progress
-            y_hat2, _ = clf_gpt2(x, training=True)
-            epoch_accuracy(y2, tf.math.softmax(y_hat2[:, -1, :]))
-
-            # End epoch
-            train_loss_results.append(epoch_loss_avg.result())
-            train_accuracy_results.append(epoch_accuracy.result())
-
-            if step % 50 == 0:
-                print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch + 1,
-                                                                            epoch_loss_avg.result(),
-                                                                            epoch_accuracy.result()))
-
-        # Evaluate
-        current_accuracy = evaluate(clf_gpt2, test_dataset)
-        if current_accuracy > best_accuracy:
-            print('\nEpoch %03d: val_acc improved from %0.5f to %0.5f,'
-                  ' saving model to %s' % (epoch + 1, best_accuracy, current_accuracy, params["check"]))
-
-            clf_gpt2.save_weights(params["check"])
-            best_accuracy = current_accuracy
 
 if __name__ == "__main__":
 
@@ -133,11 +69,11 @@ if __name__ == "__main__":
         vocab = json.load(f)
 
     # Build dataset from encoded unlabelled midis
-    train_labelled_text = load_dataset(params["train"], vocab, params["seqlen"])
-    test_labelled_text = load_dataset(params["test"], vocab, params["seqlen"])
+    train_text = load_dataset(params["train"], vocab, params["seqlen"])
+    test_text = load_dataset(params["test"], vocab, params["seqlen"])
 
-    train_sent_dataset = build_dataset(train_labelled_text, params["batch"])
-    test_sent_dataset = build_dataset(test_labelled_text, params["batch"])
+    train_dataset = build_dataset(train_text, params["batch"])
+    test_dataset = build_dataset(test_text, params["batch"])
 
     # Calculate vocab_size from char2idx dict
     vocab_size = len(vocab)
@@ -154,5 +90,7 @@ if __name__ == "__main__":
         ckpt = tf.train.Checkpoint(net=clf_gpt2)
         ckpt.restore(tf.train.latest_checkpoint(params["pretr"]))
 
-    # Train pre-trained model as a classifier
-    fit(clf_gpt2, train_sent_dataset, test_sent_dataset, params)
+    clf_gpt2.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                optimizer=tf.keras.optimizers.Adam(params["lr"]), metrics=['accuracy'])
+
+    history = clf_gpt2.fit(train_dataset, epochs=10, validation_data=test_dataset)
