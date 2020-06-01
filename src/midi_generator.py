7fd05978cfb0f442e0e54d899c4f2045341f59ae
Lucas N. Ferreira <lucasnfe@gmail.com>
2020-06-01 16:41:35 -0700

    Implemented generation loop

--- src/midi_generator.py
+++ src/midi_generator.py
@@ -6,6 +6,20 @@ import tensorflow as tf
 from clf_dnd.models import *
 from clf_vgmidi.models import *
 
+def preprocess_text(text):
+    if text == "":
+        tokens = "\n"
+    else:
+        tokens = text.split(" ")
+
+    # Remove \n
+    tokens = list(filter(lambda t: t != "\n", tokens))
+
+    # Add \n
+    tokens = ["\n"] + tokens
+
+    return " ".join(tokens)
+
 def load_language_model(vocab_size, params, path):
     # Instanciate GPT2 language model
     gpt2_config = tm.GPT2Config(vocab_size, params["seqlen"], params["n_ctx"], params["embed"], params["layers"], params["heads"],
@@ -41,16 +55,43 @@ def load_clf_vgmidi(vocab_size, params, path="../trained/clf_vgmidi.ckpt"):
 
     return clf_vgmidi
 
+def generate_midi(generation_params, language_model, clf_vgmidi, clf_dnd):
+    init_tokens = generation_params["init_tokens"]
+
+    generated = np.array([init_tokens])
+
+    while generated.shape[-1] < gen_len:
+        # Run language_model
+        generative_x = generated[:, -n_ctx:]
+        generative_y = language_model(generative_x, training=False)
+        generative_p = tf.math.softmax(generative_y)
+
+        # Get topk most likely tokens from the language model
+        top_probs, top_tokens = tf.math.top_k(generative_p, top_k)
+
+        # Create tensor with all possible tokens
+        top_tokens = np.reshape(top_tokens, (top_tokens.shape[-1], 1))
+
+        # Concatenate sequence of tokens generated so far with all possible tokens
+        generated_tiled = np.tile(generated, top_tokens.shape)
+
+        # Classifiy emotion considering all possible tokens
+        music_x = np.concatenate((generated_tiled, top_tokens), axis=1)
+        music_y = clf_vgmidi(music_x, training=False)
+        music_p = tf.math.sigmoid(music_y)
+
+    return generated
+
 if __name__ == "__main__":
     np.random.seed(42)
 
     # Parse arguments
     parser = argparse.ArgumentParser(description='midi_generator.py')
     parser.add_argument('--mode', type=str, default="sample", help="Generation strategy.")
-    parser.add_argument('--init', type=str, required=False, help="Seed text to start generation.")
+    parser.add_argument('--init', type=str, required=True, help="Seed text to start generation.")
+    parser.add_argument('--text', type=str, required=True, help="Seed text to start generation.")
     parser.add_argument('--glen', type=int, default=256, help="Length of generated midi.")
     parser.add_argument('--topk', type=int, default=10, help="Top k tokens to consider when sampling.")
-    parser.add_argument('--emo', type=int, required=False, help="Sentiment of the desired midi.")
 
     opt = parser.parse_args()
 
@@ -70,7 +111,26 @@ if __name__ == "__main__":
     language_model = load_language_model(vocab_size, params, "../trained/transformer.ckpt")
 
     # Load generative language model
-    clf_vgmidi = load_clf_vgmidi(vocab_size, params, "../trained/clf_vgmidi.ckpt/clf_vgmidi")
+    clf_vgmidi = load_clf_vgmidi(vocab_size, params, "../trained/clf_gpt2.ckpt/clf_gpt2")
 
     # Load generative language model
-    clf_dnd = load_clf_dnd(vocab_size, "../trained/clf_dnd.ckpt/clf_dnd")
+    clf_dnd = load_clf_dnd(vocab_size, "../trained/clf_bert.ckpt/clf_bert_ep1.txt/clf_bert")
+
+    # Encode init text as sequence of indices
+    init_music = preprocess_text(opt.init)
+    init_tokens = [vocab[word] for word in init_music.split(" ")]
+
+    # Compute emotion in the given story using dnd classifier
+    story_tokens = tokenizer.encode(opt.text, add_special_tokens=True)
+    story_emotion = clf_dnd(story_tokens)
+    print(story_emotion)
+
+    # Generation parameters
+    generation_params = {"init_tokens": init_tokens,
+                              "length": opt.glen,
+                                "topk": opt.topk,
+                                "sent": story_emotion}
+
+    # Generate a midi as text
+    midi_text = generate_midi(generation_params, language_model, clf_vgmidi, clf_dnd)
+    print(midi_text)
