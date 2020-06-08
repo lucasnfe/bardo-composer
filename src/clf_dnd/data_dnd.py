d049a0a4613d38eaf8e28c80b4af85646922634a
Lucas N. Ferreira <lucasnfe@gmail.com>
2020-06-08 14:06:04 -0700

    Load episodes as csv data

--- src/clf_dnd/data_dnd.py
+++ src/clf_dnd/data_dnd.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import string
 import unidecode
 from word2number import w2n
@@ -50,27 +51,22 @@ def convert_numbers(text):
 
 def load_episode(filepath):
     X, Y1, Y2 = [], [], []
-    with open(filepath) as f:
-        for line in f:
-            example = line.rsplit(":", 1)
-            x, y = example[0], example[1].replace('\n', '')
+    data = csv.DictReader(open(filepath, "r"))
+    for row in data:
+        x, y1, y2 = row["text"], int(row["valence"]), int(row["arousal"])
 
-            # Convert to lower case
-            x = x.lower()
+        # Convert to lower case
+        x = x.lower()
 
-            # Text pre-processing
-            x = remove_accented_chars(x)
-            x = remove_stopwords(x)
-            x = remove_punctuation(x)
-            x = convert_numbers(x)
+        # Text pre-processing
+        x = remove_accented_chars(x)
+        x = remove_stopwords(x)
+        x = remove_punctuation(x)
+        x = convert_numbers(x)
 
-            # Separate valence and arousal values
-            y = y.split(",")
-            y1, y2 = int(y[0]), int(y[1])
-
-            X.append(x)
-            Y1.append(y1)
-            Y2.append(y2)
+        X.append(x)
+        Y1.append(y1)
+        Y2.append(y2)
 
     return X, Y1, Y2
 
